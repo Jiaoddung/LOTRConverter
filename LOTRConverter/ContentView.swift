6813c4a4260aaf78590dace01b77739636844f26
Jiaoddung <daisy072120@gmail.com>
2024-07-28 18:40:50 -0700

    add fucking in front

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Experiment Branch!")
+            Text("Fucking Experiment Branch!")
         }
         .padding()
     }
